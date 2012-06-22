f6dbb41a33d783671754ff73c7bc2c1c25df6c38
Ben Foster <ben@planetcloud.co.uk>
2012-06-22 19:44:04 +0100

    Updated nuget packages

--- src/ImageResizer.FluentExtensions.Mvc/Properties/AssemblyInfo.cs
+++ src/ImageResizer.FluentExtensions.Mvc/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("ImageResizer.FluentExtensions.Mvc")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImageResizer.FluentExtensions.Mvc")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b34c1e63-7cd1-4411-9991-acb335b8e5a4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
