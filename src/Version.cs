dab3f0723aca6e9852fe063977f4131c7dd84bde
Ben Foster <ben@planetcloud.co.uk>
2015-05-20 11:22:40 +0100

    Upgrade to MVC5. Project re-org.

--- src/Version.cs
+++ src/Version.cs
@@ -1,17 +1,13 @@
 using System.Reflection;
-[assembly: AssemblyCompany("Ben Foster")]
-[assembly: AssemblyProduct("ImageResizer.FluentExtensions")]
-[assembly: AssemblyCopyright("Copyright © Ben Foster 2012")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyCompany("Ben Foster")]
+[assembly: AssemblyProduct("Fluent Extensions for Image Resizer")]
+[assembly: AssemblyCopyright("Copyright © Ben Foster 2015")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyInformationalVersion("1.0.0.4")]
